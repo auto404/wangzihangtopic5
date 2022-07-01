890d732dcb9400ced474976e5d8cc3db6583f50f
auto404 <3515884133@qq.com>
2022-07-01 23:26:50 +0800

    first

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请输入最终的m";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(367, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "请输入最终点n";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(176, 39);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(165, 31);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(534, 39);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(183, 31);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(757, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 48);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(16, 92);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(722, 823);
+            this.listBox1.TabIndex = 5;
+            // 
+            // chart1
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
+            this.chart1.Location = new System.Drawing.Point(790, 134);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Fire;
+            this.chart1.Size = new System.Drawing.Size(1370, 781);
+            this.chart1.TabIndex = 6;
+            this.chart1.Text = "chart1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(786, 92);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(675, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "路径权重由随机数产生，范围为0-100,0就代表相邻两点没有边不能到达";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2255, 939);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "格路问题";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public class Point
+        {// 二维坐标上的点Point
+            public int x;
+            public int y;
+            public int weight;
+        }
+
+        public class Node
+        {// 二维坐标上的点Point
+            public bool up=true;
+            public bool right=true;
+            public int  upNum=0;
+            public int rightNum=0;
+        }
+        public int sum;
+       
+        public Stack<Point> path=new Stack<Point>();
+        public Stack<Point> temp = new Stack<Point>();
+        public Stack<Point> best = new Stack<Point>();
+        public int bestWeight;
+        public int bestNum;
+        int m, n;//终点坐标
+        public List<List<Node>> nodes = new List<List<Node>>();
+        //初始化节点表格,某单元格的表示为（nodes[y][x]）上为y正方向，右为x正方向
+        void getform()
+        {
+           
+            for (int i = 0; i <= m; i++)
+            {
+                 Random rd = new Random(Guid.NewGuid().GetHashCode());
+                 List<Node> list = new List<Node>();
+                 for (int j = 0; j <=n; j++)
+                 {
+                    
+                    Node node = new Node();
+                    node.rightNum=rd.Next(0,101);
+                    node.upNum=rd.Next(0,101);
+                    if(node.upNum==0)
+                    {
+                        node.up = false;
+                    }
+                    if (node.rightNum == 0)
+                    {
+                        node.right = false;
+                    }
+                    if (j == n)
+                    {
+                        node.upNum = 0;
+                       node.up = false;
+                    }
+                        
+                   if(i==m)
+                    {
+                        node.rightNum = 0;
+                       node.right = false;
+                    }
+                       
+                   list.Add(node);
+                  }
+                   nodes.Add(list);
+            }
+           
+        }
+        void getSum(int a, int b)
+        {
+
+            if (m== a && n == b)
+            {
+                int weight=0;
+        
+                Point p = new Point();
+                p.x = a;
+                p.y = b;
+                path.Push(p);//让当前点进栈
+                listBox1.Items.Add("******************路径" + ++sum + "******************" );
+               
+                while (path.Count!=0)//将path里面的点取出来，放在temp里面
+                {//path从栈顶-栈底的方向，路径是从终点-起点的顺序
+                    Point p1 = path.Pop();
+                    temp.Push(p1);
+                }
+                while (temp.Count!=0)
+                {//输出temp里面的路径，这样刚好是从起点到终点的顺序
+                    Point p1 = temp.Pop();
+                    path.Push(p1);//将路径放回path里面，因为后面还要回溯!!!
+                    listBox1.Items.Add("(" + p1.x + "," +p1.y + ")");
+                   
+                    weight += p1.weight;
+                }
+               
+                listBox1.Items.Add("权重" + weight);
+                if (sum == 1)
+                {
+                    bestWeight = weight;
+                    bestNum = 1;
+                    while (path.Count != 0)//将path里面的点取出来，放在temp里面
+                    {//path从栈顶-栈底的方向，路径是从终点-起点的顺序
+                        Point p1 = path.Pop();
+                        temp.Push(p1);
+                    }
+                    while (temp.Count != 0)
+                    {//输出temp里面的路径，这样刚好是从起点到终点的顺序
+                        Point p1 = temp.Pop();
+                        path.Push(p1);//将路径放回path里面，因为后面还要回溯!!!
+                        best.Push(p1);
+                    }
+                }
+                if(bestWeight > weight)
+                {
+                    bestWeight=weight;
+                    bestNum = sum;
+                    best.Clear();
+                    while (path.Count != 0)//将path里面的点取出来，放在temp里面
+                    {//path从栈顶-栈底的方向，路径是从终点-起点的顺序
+                        Point p1 = path.Pop();
+                        temp.Push(p1);
+                    }
+                    while (temp.Count != 0)
+                    {//输出temp里面的路径，这样刚好是从起点到终点的顺序
+                        Point p1 = temp.Pop();
+                        path.Push(p1);//将路径放回path里面，因为后面还要回溯!!!
+                        best.Push(p1);
+                    }
+                }
+                path.Pop();
+                return;
+            }
+            if (nodes[a][b].up)
+            {
+                Point p = new Point();
+                p.x = a;
+                p.y = b;
+                p.weight = nodes[a][b].upNum;
+                path.Push(p);//让当前点进栈
+                getSum(a, b + 1);
+                
+                path.Pop();
+            }
+            if (nodes[a][b].right)
+            {
+                Point p = new Point();
+                p.x = a;
+                p.y = b;
+                p.weight = nodes[a][b].rightNum;
+                path.Push(p);//让当前点进栈
+                getSum(a + 1, b);
+           
+                path.Pop();
+            }
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sum = 0;
+            nodes.Clear();
+            chart1.Series.Clear();
+            best.Clear();
+            bestNum = 0;
+            bestWeight = 0;
+            m = int.Parse(textBox1.Text.Trim());
+            n = int.Parse(textBox2.Text.Trim());
+            getform();
+            getSum(0, 0);
+            System.Windows.Forms.DataVisualization.Charting.Series series = new System.Windows.Forms.DataVisualization.Charting.Series();
+            listBox1.Items.Add("******************最优路径" +bestNum + "******************");
+            while (best.Count != 0)//将path里面的点取出来，放在temp里面
+            {//path从栈顶-栈底的方向，路径是从终点-起点的顺序
+                Point p1 = best.Pop();
+                temp.Push(p1);
+            }
+            while (temp.Count != 0)
+            {//输出temp里面的路径，这样刚好是从起点到终点的顺序
+                Point p1 = temp.Pop();
+                best.Push(p1);//将路径放回path里面，因为后面还要回溯!!!
+                series.Points.AddXY(p1.x, p1.y);
+                listBox1.Items.Add("(" + p1.x + "," + p1.y + ")");
+            }
+            series.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.StepLine;
+            series.Name = "最优路径" + bestNum + " 权重为" + bestWeight;
+            chart1.Series.Add(series);
+            listBox1.Items.Add("权重" + bestWeight);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treeView2_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+    }
+}
